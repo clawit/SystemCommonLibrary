3947bd27162f1dc32a03d7d5a288bf8e0037e0d2
Kevin <kevinyeti@gmail.com>
2018-04-18 09:47:53 +0800

    0.1.0.0

--- src/SystemCommonLibrary/Network/Http.cs
+++ src/SystemCommonLibrary/Network/Http.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Web;
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace SystemCommonLibrary.Network
 {
-	public class Http
+    public class Http
 	{
-		public static string GetIP (HttpRequest request)
+		public static string GetIp (HttpRequest request)
 		{
-			if (request.ServerVariables ["HTTP_VIA"] != null) // using proxy
-			{
-				return request.ServerVariables ["HTTP_X_FORWARDED_FOR"].ToString ();  // Return real client IP.
-			} else// not using proxy or can't get the Client IP
-			  {
-				return request.ServerVariables ["REMOTE_ADDR"].ToString (); //While it can't get the Client IP, it will return proxy IP.
-			}
-		}
-	}
+            string ip = "0.0.0.0";
+            if (request == null || request.Headers.Count == 0
+                || request.Headers["X-Forwarded-For"].Count == 0)
+            {
+                return ip;
+            }
+            else
+            {
+                ip = request.Headers["X-Forwarded-For"].FirstOrDefault();
+                if (string.IsNullOrEmpty(ip))
+                    ip = "0.0.0.0";
+            }
+
+            return ip;
+        }
+    }
 }
