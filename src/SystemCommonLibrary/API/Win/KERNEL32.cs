ccb22321f730c1b7d2d0f792d26836e775eccc1e
Kevin <kevinyeti@gmail.com>
2018-07-09 18:33:48 +0800

    update v0.2.0.2

---
+++ src/SystemCommonLibrary/API/Win/KERNEL32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SystemCommonLibrary.API.Win
+{
+    public class KERNEL32
+    {
+        private const string KERNEL32_DLL = "KERNEL32.DLL";
+         
+        [DllImport(KERNEL32_DLL)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ src/SystemCommonLibrary/API/Win/USER32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SystemCommonLibrary.API.Win
+{
+    public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    /// <summary>
+    /// Helper class containing Gdi32 API functions
+    /// </summary>
+    public class USER32
+    {
+        private const string USER32_DLL = "USER32.DLL";
+         
+
+        [DllImport(USER32_DLL)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport(USER32_DLL)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport(USER32_DLL)]
+        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+    }
+}
