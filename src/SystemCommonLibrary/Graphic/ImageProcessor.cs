c87a30ccf117824e973fdc8d0456173cc62d70a6
shenyi <kevinyeti@gmail.com>
2020-06-11 18:40:45 +0800

    add image processor

---
+++ src/SystemCommonLibrary/Graphic/ImageProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCommonLibrary.Graphic
+{
+    public static class ImageProcessor
+    {
+        public static Image ReadFromFile(string filename)
+        {
+            return new Bitmap(filename);        
+        }
+
+        public static MemoryStream AddWatermark(Image bitmap, string watermark, Font font = null)
+        {
+            var stream = new MemoryStream();
+            var image = new Bitmap(bitmap);
+            using (var graphics = Graphics.FromImage(image))
+            {
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+
+                if (font == null)
+                {
+                    font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                }
+                var size = graphics.MeasureString(watermark, font);
+
+                var color = Color.FromArgb(128, 255, 255, 255);
+                var brush = new SolidBrush(color);
+                var pos = new PointF((image.Width - size.Width)/2, (image.Height - size.Height)/2 - 30);
+
+                graphics.DrawString(watermark, font, brush, pos);
+                image.Save(stream, ImageFormat.Jpeg);
+
+                return stream;
+            }
+        }
+
+        public static string ImgToBase64String(MemoryStream stream)
+        {
+            byte[] bytes = new byte[stream.Length];
+            stream.Position = 0;
+            stream.Read(bytes, 0, (int)stream.Length);
+            stream.Close();
+
+            return "data:image/jpeg;base64," + Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ src/UnitTest/ImageTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using SystemCommonLibrary.Attribute;
+using SystemCommonLibrary.Graphic;
+using SystemCommonLibrary.Serialization;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ImageTest
+    {
+        [Fact]
+        public void Image2Base64Test()
+        {
+            var img = ImageProcessor.ReadFromFile("TestData/mount.jpg");
+            var ms = ImageProcessor.AddWatermark(img, "TUUTKUˮӡ");
+            string s = ImageProcessor.ImgToBase64String(ms);
+
+            
+        }
+    }
+}
