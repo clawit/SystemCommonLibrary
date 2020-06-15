38605e3e7b2a054c84b8a078a91b5f477470cd4d
shenyi <kevinyeti@gmail.com>
2020-06-15 22:12:27 +0800

    0.4.0.0

---
+++ src/SystemCommonLibrary/Data/Connector/MySqlConnector.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SystemCommonLibrary.Data.Connector
+{
+    /// <summary>
+    /// mysql数据库访问器
+    /// </summary>
+    public static class MySqlConnector
+    {
+
+        /// <summary>
+        /// 打开连接
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static IDbConnection Open(string name)
+        {
+            try
+            {
+                IDbConnection _db = new MySqlConnection(ConfigurationManager.ConnectionStrings[name].ConnectionString);
+                _db.Open();
+                return _db;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
