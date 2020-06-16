c8e21fe04421e419d11369079ec0e1f439784dac
shenyi <kevinyeti@gmail.com>
2020-06-16 18:39:22 +0800

    0.4.0.1

---
+++ src/SystemCommonLibrary.AspNetCore/Json/DynamicJsonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemCommonLibrary.AspNetCore.Json
+{
+    public static class DynamicJsonExtensions
+    {
+        public static IMvcBuilder AddDynamicJson(this IMvcBuilder builder)
+        {
+            return builder.AddMvcOptions(option =>
+            {
+                option.InputFormatters.Clear();
+                option.InputFormatters.Add(new DynamicJsonInputFormatter());
+            });
+        }
+    }
+}
---
+++ src/SystemCommonLibrary.AspNetCore/Json/DynamicJsonInputFormatter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.IO;
+using System.Threading.Tasks;
+using SystemCommonLibrary.Json;
+
+namespace SystemCommonLibrary.AspNetCore.Json
+{
+    public sealed class DynamicJsonInputFormatter : IInputFormatter
+    {
+        public bool CanRead(InputFormatterContext context) => context.HttpContext.Request.ContentType.StartsWith("application/json");
+
+        public async Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            if (request.Body.CanSeek && request.Body.Length == 0)
+                return await InputFormatterResult.NoValueAsync();
+
+            var reader = new StreamReader(request.Body);
+            string json = await reader.ReadToEndAsync();
+
+            var result = DynamicJson.Parse(json);
+            return await InputFormatterResult.SuccessAsync(result);
+        }
+    }
+}
