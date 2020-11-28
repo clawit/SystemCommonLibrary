3095fa709ca72d3af7e3b2947045e3eef713d21a
YvesShen <KevinYeti@gmail.com>
2020-11-28 14:55:17 +0800

    add redis open database

--- src/SystemCommonLibrary.Data/Connector/RedisConnector.cs
+++ src/SystemCommonLibrary.Data/Connector/RedisConnector.cs
@@ -10,25 +10,34 @@ namespace SystemCommonLibrary.Data.Connector
         //保存连接以复用
         //ref:https://stackexchange.github.io/StackExchange.Redis/Basics
         private static Dictionary<string, ConnectionMultiplexer> dicPool = new Dictionary<string, ConnectionMultiplexer>();
+
         /// <summary>
         /// 打开连接
         /// </summary>
-        /// <param name="connName">数据库名称</param>
+        /// <param name="connStr">数据库连接</param>
         /// <returns></returns>
-        public static IConnectionMultiplexer Open(string connName = "redis")
+        public static IConnectionMultiplexer Open(string connStr = "redis")
         {
             lock (dicPool)
             {
-                if (!dicPool.ContainsKey(connName))
+                if (!dicPool.ContainsKey(connStr))
                 {
-                    string redisConnectionStr = ConfigurationManager.ConnectionStrings[connName].ConnectionString;
-                    var conn = ConnectionMultiplexer.Connect(redisConnectionStr);
-                    dicPool.Add(connName, conn);
+                    var conn = ConnectionMultiplexer.Connect(connStr);
+                    dicPool.Add(connStr, conn);
                 }
-                return dicPool[connName];
+                return dicPool[connStr];
             }
+        }
 
-          
+        /// <summary>
+        /// 打开连接,获得数据库
+        /// </summary>
+        /// <param name="connStr">数据库连接</param>
+        /// <param name="db">数据库index</param>
+        /// <returns></returns>
+        public static IDatabase Open(string connStr, int db = -1)
+        {
+            return Open(connStr).GetDatabase(db);
         }
     }
 }
