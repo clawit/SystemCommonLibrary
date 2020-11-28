6706899fc0157aeee6310973554fd52e4554e904
YvesShen <KevinYeti@gmail.com>
2020-11-28 14:54:58 +0800

    add DateTimeConvertor

---
+++ src/SystemCommonLibrary/Convertion/DateTimeConvertor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SystemCommonLibrary.Convertion
+{
+    public static class DateTimeConvertor
+    {
+        public static double ToUnixEpoch(this DateTime dateTime)
+        {
+            return (dateTime - DateTime.UnixEpoch).TotalSeconds;
+        }
+
+        public static DateTime FromUnixEpoch(this double unitTimestamp)
+        {
+            return DateTime.UnixEpoch.AddSeconds(unitTimestamp);
+        }
+
+    }
+}
