963151cd5d61802c5709e364663ac3bbab75b5c1
YvesShen <KevinYeti@gmail.com>
2020-11-28 22:40:35 +0800

    DateTimeConvertExtensions bug fix

---
+++ src/SystemCommonLibrary/Convertion/DateTimeConvertExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SystemCommonLibrary.Convertion
+{
+    public static class DateTimeConvertExtensions
+    {
+        public static double ToUnixEpoch(this DateTime dateTime)
+        {
+            return (dateTime - DateTime.UnixEpoch.ToLocalTime()).TotalSeconds;
+        }
+
+        public static DateTime ToLocalTime(this double unitTimestamp)
+        {
+            return DateTime.UnixEpoch.ToLocalTime().AddSeconds(unitTimestamp);
+        }
+
+    }
+}
---
+++ src/UnitTest/DateTimeConvertExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SystemCommonLibrary.Convertion;
+
+namespace UnitTest
+{
+    public class DateTimeConvertExtensionsTest
+    {
+        [Fact]
+        public void ToUnixEpoch_Test()
+        {
+            DateTime dt = new DateTime(2020, 11, 28, 21, 54, 32, 321, DateTimeKind.Local);
+            var unixTimestamp = dt.ToUnixEpoch();
+
+            Assert.Equal(1606571672.321, unixTimestamp);
+        }
+
+        [Fact]
+        public void ToLocalTime_test()
+        {
+            var dt = 1606571672.321.ToLocalTime();
+            DateTime expected = new DateTime(2020, 11, 28, 21, 54, 32, 321, DateTimeKind.Local);
+
+            Assert.Equal(expected, dt);
+        }
+    }
+}
