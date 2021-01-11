07c34da6c893913e5bcaf2a205d0ad61fb67314f
YvesShen <KevinYeti@gmail.com>
2021-01-11 22:23:24 +0800

    0.5.7.6

--- src/SystemCommonLibrary.Data/DataEntity/EditorAttribute.cs
+++ src/SystemCommonLibrary.Data/DataEntity/EditorAttribute.cs
@@ -7,7 +7,7 @@ namespace SystemCommonLibrary.Data.DataEntity
     [AttributeUsage(AttributeTargets.Property)]
     public class EditorAttribute : Attribute
     {
-        public bool Editable { get; } = true;
+        public bool Editable { get; set; } = true;
         public EditorType EditorType { get; }
 
         public EditorAttribute(EditorType type)
