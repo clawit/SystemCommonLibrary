7fd7d9590a520977f16349bbffd78606f0008f1f
YvesShen <KevinYeti@gmail.com>
2021-01-11 13:20:52 +0800

    0.5.7.2

--- src/SystemCommonLibrary.Data/DataEntity/Entity.cs
+++ src/SystemCommonLibrary.Data/DataEntity/Entity.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace SystemCommonLibrary.Data.DataEntity
 {
     public abstract class Entity
     {
         [Key]
+        [Display(Name = "序号")]
         public int Id { get; set; }
 
+        [Display(Name = "创建时间")]
         public DateTime CreateAt { get; set; }
 
+        [Display(Name = "创建者")]
         public string Creator { get; set; }
     }
 }
