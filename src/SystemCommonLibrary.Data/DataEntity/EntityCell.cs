dfedfa6d5ed7b6b0b8c15486113db3dfab72fb69
YvesShen <KevinYeti@gmail.com>
2021-03-26 15:17:05 +0800

    0.6.0.11

--- src/SystemCommonLibrary.Data/DataEntity/EntityCell.cs
+++ src/SystemCommonLibrary.Data/DataEntity/EntityCell.cs
@@ -4,6 +4,15 @@ namespace SystemCommonLibrary.Data.DataEntity
 {
     public class EntityCell
     {
+        public EntityCell() { }
+
+        public EntityCell(EntityColumn column, object key, object value)
+        {
+            Column = column;
+            Key = key;
+            Value = value;
+        }
+
         public EntityColumn Column { get; set; }
 
         public object Key { get; set; }
--- src/SystemCommonLibrary.Data/DataEntity/EntitySchema.cs
+++ src/SystemCommonLibrary.Data/DataEntity/EntitySchema.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace SystemCommonLibrary.Data.DataEntity
 {
     public class EntitySchema
     {
         public List<EntityColumn> Columns { get; set; } = new List<EntityColumn>();
+
+        public EntityColumn GetEntityKey()
+        {
+            if (Columns.Count > 0)
+            {
+                var key = Columns.FirstOrDefault(c => c.IsEntityKey);
+                if (key == null)
+                {
+                    key = Columns.FirstOrDefault(c => c.IsKey);
+                }
+
+                return key;
+            }
+            else
+                return null;
+        }
     }
 }
