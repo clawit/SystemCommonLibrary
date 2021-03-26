0ee804fc0f52faf2e7c9e366a533bac67264cad8
YvesShen <KevinYeti@gmail.com>
2021-03-26 14:42:27 +0800

    0.6.0.10
    add EntityCell

---
+++ src/SystemCommonLibrary.Data/DataEntity/EntityCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SystemCommonLibrary.Data.DataEntity
+{
+    public class EntityCell
+    {
+        public EntityColumn Column { get; set; }
+
+        public object Key { get; set; }
+
+        public object Value { get; set; }
+    }
+}
